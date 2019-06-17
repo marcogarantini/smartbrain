5a6f14ac5f6aac41e9b12cd1dce8a39871cd0a76
Ubuntu <ubuntu@ip-172-31-90-117.ec2.internal>
2019-06-17 23:28:52 +0000

    Updates

---
+++ precache-manifest.16739862958eaae528ed6bab2b993507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2b805c0e3a63cdd7e7b287b4742603",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "/smartbrain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b02b628485a137553a93",
+    "url": "/smartbrain/static/css/main.617fa1be.chunk.css"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "/smartbrain/static/js/2.33497142.chunk.js"
+  },
+  {
+    "revision": "b02b628485a137553a93",
+    "url": "/smartbrain/static/js/main.ebe8b421.chunk.js"
+  },
+  {
+    "revision": "c811ced7da05cfde01c3",
+    "url": "/smartbrain/static/js/runtime~main.54e658c3.js"
+  }
+]);
